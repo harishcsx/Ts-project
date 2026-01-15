3254438788f6a92aa76623687c9f51d7e00a336b
harishcsx <csx.harish@gmail.com>
2026-01-15 23:02:51 +0530

    implement isadmin functionality

--- src/p1/smart_user_validator.ts
+++ src/p1/smart_user_validator.ts
@@ -12,6 +12,30 @@ type Admin = {
 
 type User = Guest | Admin;
 
-function parserUser(value: unknown): value as User{
-    
+function parserUser(value: unknown): User{
+    return true
 }
+
+// supporting function     
+
+function isAdmin(value: unknown): value is Admin{
+    if(typeof value !== "object") return false;
+     
+
+    const valueObj = value as {
+        role?: string
+        permission?: string[]
+    };
+
+    if(valueObj.role !== "string") return false;
+    if(!Array.isArray(valueObj.permission)) return false;
+    return valueObj.permission.every(p => typeof p === "string");
+
+}
+
+function isGuest(value: unknown): value is Guest{
+    return true;
+    // if date is in string convert it into date 
+}
+
+
